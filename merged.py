4adc57a5ca3899088fe50775544dbecb93dd1539
Siddharth <soma_841975@student.nitw.ac.in>
2021-01-02 17:05:52 +0530

    Autoname the save file issue soln

--- merged.py
+++ merged.py
@@ -26,6 +26,18 @@ def merged(alpha1,alpha2):
     new_image.paste(img1 ,(0,0))
     new_image.paste(img2,(img1_size[0],0))
     new_image.save("images/save/newww.jpg","JPEG")
+    
+    #This will rename all the files as and when they are added and the program runs.
+    path = os.chdir(#Path to save folder
+        )
+
+    i = 1
+    for file in os.listdir(path):
+        new_file_name = "pic{}.jpg".format(i)
+        os.rename(file , new_file_name)
+
+	i += 1
+
     new_image.show()
 
 
